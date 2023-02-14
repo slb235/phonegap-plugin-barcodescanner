4e4124979ea8cf5d03be251f8f582013f5aa9cec
Jonathan Rauprich <rauprich.jonathan@googlemail.com>
2023-02-14 16:54:26 +0100

    Update CDVBarcodeScanner.mm

--- src/ios/CDVBarcodeScanner.mm
+++ src/ios/CDVBarcodeScanner.mm
@@ -358,9 +358,7 @@ parentViewController:(UIViewController*)parentViewController
 
 //--------------------------------------------------------------------------
 - (void)openDialog {
-    if (@available(iOS 13.0, *)) {
-        [self.viewController setModalPresentationStyle:UIModalPresentationFullScreen];
-    }
+    [self.viewController setModalPresentationStyle:UIModalPresentationFullScreen];
     [self.parentViewController
      presentViewController:self.viewController
      animated:self.isTransitionAnimated completion:nil
